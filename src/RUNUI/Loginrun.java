ca63e5d4b19303e914b7b1b475103c449f970f72
dhis04 <hieund.22it@vku.udn.vn>
2023-02-05 04:39:23 +0700

    update

---
+++ src/RUNUI/Loginrun.java
@@ -0,0 +1,11 @@
+package RUNUI;
+
+import UI.Login;
+
+public class Loginrun {
+    public static void main(String[] args) {
+       Login log = new Login("Log In");
+       log.showWh();
+    }
+    
+}
